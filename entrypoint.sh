a506e125736e65baea2eb62492b1cd3d4bf9b2f8
Mona Lisa <Mona Lisa>
2025-10-07 16:33:28 +0700

    run consumer

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ollama serve & uv run uvicorn app.main:app --host 0.0.0.0 --port 8081
+ollama serve & uv run uvicorn app.main:app --host 0.0.0.0 --port 8081 $ uv run python -m app.consumer
