2a0dd70d1229ef343339d845769d427d95004589
Mona Lisa <Mona Lisa>
2026-03-11 09:17:56 +0700

    add run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,18 @@
-docker run --rm --privileged --gpus "device=0" --shm-size="16g" --name resume_analysis \
--p 9001:9001 \
-yaibawiliam/resume_analysis:$1
+# docker run --rm --privileged --gpus "device=0" --shm-size="16g" --name resume_analysis \
+# -p 9001:9001 \
+# yaibawiliam/resume_analysis:$1
 
 
 # docker run --rm --gpus all --shm-size="8g" --name resume_analysis -p 9001:9001 yaibawiliam/resume_analysis:$1
 # --runtime=nvidia
+
+#!/bin/bash
+
+VERSION=${1:-$(date +%Y%m%d%H%M%S)}
+echo "🚀 Starting deploy with version: $VERSION"
+docker build -t my-app-image:$VERSION .
+
+echo "🚢 Running docker compose ..."
+APP_VERSION=$VERSION docker-compose up -d
+
+echo "✅ Deploy done"
